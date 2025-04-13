8a34e8cb23567eac6d73ee7c2aa200e6bcb4da73
Zugarramurdi <juanma.segura.canales@gmail.com>
2025-04-13 16:04:40 +0200

    #00 - javascript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Zugarramurdi.js
@@ -0,0 +1,28 @@
+// Documentacion de JavaScript: https://developer.mozilla.org/es/docs/Web/JavaScript
+// Estandares de JavaScript: https://ecma-international.org/publications-and-standards/standards/
+
+// Esto es un comentario de una linea
+/*
+  Esto es un comentario
+  de varias
+  lineas, se inicia con "/*"
+  y termina con */
+
+// Declaracion de variables
+var variable1; // Declaracion de una variable
+let variable2; // Declaracion de una variable local de bloque
+const constante = 10; // Declaracion de una constante
+
+
+// Tipos de datos primitivos de JavaScript
+let number = 28; // Number, numero entero
+let str = "Cadena de caracteres"; // String, cadena de caracteres
+let float = 3.14; // Float, numero decimal
+let bigInt = 1234567890123456789012345678901234567890n; // BigInt, numero entero extremadamente grande
+let bool = true; // Boolean, verdadero o falso
+let _null = null; // Null, ausencia de valor
+let _undefined; // Undefined, variable no definida
+let symbol = Symbol("descripcion"); // Symbol, valor unico e inmutable
+
+// Imprimir en consola
+console.log("Hola, JavaScript!") // Imprime "Hola, JavaScript!" en la consola
